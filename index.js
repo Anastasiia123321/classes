d5c938547cc6f246eafdffbc12d4502c49c7f914
Anastasiia123321 <60201048+Anastasiia123321@users.noreply.github.com>
2020-03-02 21:04:15 +0200

    Update index.js

--- index.js
+++ index.js
@@ -60,8 +60,10 @@ class BudgetStudent extends Student{
   }
 scholarshipInterval() {
     setInterval(() => {(this.getScholarship(),30000);
-)}
+  }
+ }
 }
+
 const anastasiia = new BudgetStudent('Студентка 5 курсу', 'ЛНУ ім. І.Франка', 'Михальчук Анастасія');
 console.log(anastasiia.getInfo());
 
